e8c06a13f0f0bd8a28fa2e862dcdeb5a5e8b1141
karinlee <karinlee92@gmail.com>
2023-01-06 12:03:36 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let varName = "SuperCoder. "
+
+//paramName is a parameter
+function intro2(paramName){
+console.log("Hi, " + varName + "Mein Name ist " + paramName + " .");
+}
+
+intro2("Karin"); //"Karin" is a argument
